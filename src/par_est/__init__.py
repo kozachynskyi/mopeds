c8751ab220db7fb5667d1ce68de2b9a7fc3b6d70
Volodymyr Kozachynskyi <vovakozach@gmail.com>
2022-02-19 18:23:43 +0100

    feat: add SImulatorNLE to __init__

--- src/par_est/__init__.py
+++ src/par_est/__init__.py
@@ -12,7 +12,7 @@ from .variables import BadVariableError
 from .variables import ORIGIN_TS
 from .tools import MXPickler
 from .model import Model
-from .simulation import Simulator
+from .simulation import Simulator, SimulatorNLE
 from .optimization import (
     Optimizer,
     ParameterEstimation,
