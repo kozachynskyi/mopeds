1fb5bb16bd923131bc22ce45c3c775989f6f246c
Volodymyr Kozachynskyi <vovakozach@gmail.com>
2022-03-15 14:55:02 +0100

    feat: remove setup.py

--- setup.py
+++
@@ -1,6 +0,0 @@
-# This file is only need, if user wants to install this package
-# on local PC with pip install -e "path/to/par_est"
-
-import setuptools
-
-setuptools.setup(name="par_est", package_dir={"": "src"})
