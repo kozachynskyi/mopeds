b467503cb48513471897a857fe3bbec92ce02bc7
Dario Staubach <d.staubach@campus.tu-berlin.de>
2022-08-18 14:19:21 +0200

    feat: added examples for calculate_inference_bound based on Bates et al.

--- examples/nle/BOD_inference.py
+++ examples/nle/BOD_inference.py
@@ -1,63 +1,46 @@
-import par_est
 import matplotlib.pyplot as plt
-from copy import deepcopy
-import numpy as np
-from scipy.stats import f
+
+import par_est
 
 VAR_LIST, MODEL, EXP_DATA = par_est.examples.bod_model()
 
-VAR_LIST["theta1"].value = 19.143
-VAR_LIST["theta2"].value = 0.5311
+dict_of_params = {
+    "theta1": 19.143,
+    "theta2": 0.5311,
+}
 
-time_exp = []
-fun = []
-fun_sim = []
-for experiment in EXP_DATA:
-    experiment["theta1"].value = 19.143
-    experiment["theta2"].value = 0.5311
-    time_exp.append(experiment.dataframe["x"].to_numpy())
-    fun.append(experiment.dataframe["f"].to_numpy())
+dict_of_controls = {
+    "x": [0.0, 8.0, 90 + 1],
+}
 
-simulation_var_lists = []   
-time_grid = np.linspace(0, 8, 33, endpoint=True)   
-for time in time_grid:
-    simulation_var_list = deepcopy(VAR_LIST)
-    simulation_var_list['x'].value = time
-    simulation_var_list['f'].value = 1
-    for var in simulation_var_list.values():
-        var.fixed = True
-        
-    res = par_est.SimulatorNLE(MODEL, simulation_var_list).simulate_sym()   
-    fun_sim.append(res["x"].toarray().flatten()) 
-    
-    simulation_var_list["theta1"].fixed = False
-    simulation_var_list["theta2"].fixed = False
-    simulation_var_lists.append(simulation_var_list)
-    
+dict_of_responses = {
+    "f": 1e1,
+}
+
 pe = par_est.ParameterEstimationNLE(MODEL, EXP_DATA)
-pe_grid = par_est.ParameterEstimationNLE(MODEL, simulation_var_lists)
-param_dict = {}
-for param_name in list(pe.varlist_parameter.keys()
-):  
-    param_dict[param_name] = float(EXP_DATA[0].dataframe[param_name].to_numpy()[0])
+exp_inference_results, exp_data, sim_data = pe.calculate_inference_bounds(
+    dict_of_params, dict_of_responses, dict_of_controls
+)
 
-OLS = pe.calculate_objective_and_residual(param_dict)["f"]
-jac = pe.calculate_sensitivity_and_fim(param_dict,list(pe.varlist_parameter.keys()))["jac_sorted"]["f"]
-jac_grid = pe_grid.calculate_sensitivity_and_fim(param_dict,list(pe.varlist_parameter.keys()))["jac_sorted"]["f"]
-len_exp = len(EXP_DATA)
-len_param = len(param_dict)
-DOF = len_exp-len_param
-s = np.sqrt(OLS/DOF)
-R = np.linalg.qr(jac, mode="reduced")[1]
-fisher95 = f(len_param, DOF).ppf(0.95)
-bound_array = s*np.linalg.norm(jac_grid@np.linalg.inv(R), axis=1)*np.sqrt(len_param*fisher95)
-time_grid = np.hstack(time_grid)
-fun_sim = np.hstack(fun_sim)
-    
-plt.plot(time_exp, fun, "ko", label="Exp. data")
-plt.plot(time_grid, fun_sim, "k-", label="Sim. data")
-plt.plot(time_grid, fun_sim-bound_array, "b:", label="Lower bound")
-plt.plot(time_grid, fun_sim+bound_array, "r:", label="Upper bound")
+plt.plot(exp_data["x"], exp_data["f"], "ko", label="Exp. data")
+plt.plot(
+    exp_inference_results["x"],
+    exp_inference_results["f"]["simulation"],
+    "k-",
+    label="Sim. data",
+)
+plt.plot(
+    exp_inference_results["x"],
+    exp_inference_results["f"]["lower bound"],
+    "b:",
+    label="Lower bound",
+)
+plt.plot(
+    exp_inference_results["x"],
+    exp_inference_results["f"]["upper bound"],
+    "r:",
+    label="Upper bound",
+)
 plt.legend()
 plt.xlim(0, 8)
 plt.xlabel("Time")
@@ -65,3 +48,44 @@ plt.ylim(-5, 30)
 plt.ylabel("Oxygen demand")
 plt.grid()
 plt.show()
+
+dict_of_artificial_controls = {
+    "x": [0.0, 8.0, 10 + 1],
+}
+(
+    artificial_inference_results,
+    artificial_data,
+    artificial_data_sim,
+) = pe.calculate_inference_bounds(
+    dict_of_params, dict_of_responses, dict_of_controls, dict_of_artificial_controls
+)
+
+plt.plot(artificial_data["x"], artificial_data["f"], "ko", label="Artifial data")
+plt.plot(
+    artificial_inference_results["x"],
+    artificial_inference_results["f"]["simulation"],
+    "k-",
+    label="Sim. data",
+)
+plt.plot(
+    artificial_inference_results["x"],
+    artificial_inference_results["f"]["lower bound"],
+    "b:",
+    label="Lower bound",
+)
+plt.plot(
+    artificial_inference_results["x"],
+    artificial_inference_results["f"]["upper bound"],
+    "r:",
+    label="Upper bound",
+)
+plt.legend()
+plt.xlim(0, 8)
+plt.xlabel("Time")
+plt.ylim(-5, 30)
+plt.ylabel("Oxygen demand")
+plt.grid()
+plt.show()
+
+print(exp_inference_results["f"]["s"])
+print(artificial_inference_results["f"]["s"])
--- examples/nle/puromycin_inference.py
+++ examples/nle/puromycin_inference.py
@@ -1,62 +1,48 @@
-import par_est
 import matplotlib.pyplot as plt
-from copy import deepcopy
-import numpy as np
-from scipy.stats import f
+
+import par_est
 
 VAR_LIST, MODEL, EXP_DATA = par_est.examples.puromycin_model()
 
 data = EXP_DATA["Treated"]
-time_exp = []
-fun = []
-fun_sim = []
-for experiment in data:
-    time_exp.append(experiment.dataframe["x"].to_numpy())
-    fun.append(experiment.dataframe["f"].to_numpy())
-   
-concentration_grid = np.linspace(0, 1.2, 49, endpoint=True)  
-simulation_var_lists = [] 
-for concentration in concentration_grid:
-    simulation_var_list = deepcopy(VAR_LIST)
-    simulation_var_list['x'].value = concentration
-    simulation_var_list['f'].value = 1
-    for var in simulation_var_list.values():
-        var.fixed = True
-        
-    res = par_est.SimulatorNLE(MODEL, simulation_var_list).simulate_sym()   
-    simulation_var_list["theta1"].fixed = False
-    simulation_var_list["theta2"].fixed = False
-    simulation_var_lists.append(simulation_var_list)
-    fun_sim.append(res["x"].toarray().flatten()) 
-    
-pe = par_est.ParameterEstimationNLE(MODEL, data)
-pe_grid = par_est.ParameterEstimationNLE(MODEL, simulation_var_lists)
-param_dict = {}
-for param_name in list(pe.varlist_parameter.keys()
-):  
-    param_dict[param_name] = float(data[0].dataframe[param_name].to_numpy()[0])
+list_of_params = list(["theta1", "theta2"])
 
-OLS = pe.calculate_objective_and_residual(param_dict)["f"]
-sens_info = pe.calculate_sensitivity_and_fim(param_dict,list(pe.varlist_parameter.keys()))
-sens_info_grid = pe_grid.calculate_sensitivity_and_fim(param_dict,list(pe.varlist_parameter.keys()))
-len_exp = len(data)
-len_param = len(param_dict)
-jac_grid = sens_info_grid["jac_full"]
-jac_grid_sorted = sens_info_grid["jac_sorted"]["f"]
-print(jac_grid_sorted)
-DOF = len_exp-len_param
-s = np.sqrt(OLS/DOF)
-R = np.linalg.qr(sens_info["jac_full"], mode="reduced")[1]
-fisher95 = f(len_param, DOF).ppf(0.95)
-val = jac_grid@np.linalg.inv(R)
-bound_array = s*np.linalg.norm(jac_grid@np.linalg.inv(R), axis=1)*np.sqrt(len_param*fisher95)
-concentration_grid = np.hstack(concentration_grid)
-fun_sim = np.hstack(fun_sim)
-    
-plt.plot(time_exp, fun, "ko", label="Exp. data")
-plt.plot(concentration_grid, fun_sim, "k-", label="Sim. data")
-plt.plot(concentration_grid, fun_sim-bound_array, "b:", label="Lower bound")
-plt.plot(concentration_grid, fun_sim+bound_array, "r:", label="Upper bound")
+dict_of_params = {}
+for param in list_of_params:
+    dict_of_params[param] = float(data[0][param].value[0])
+
+dict_of_controls = {
+    "x": [0.0, 1.2, 120 + 1],
+}
+
+dict_of_responses = {
+    "f": 5e1,
+}
+
+pe = par_est.ParameterEstimationNLE(MODEL, data)
+exp_inference_results, exp_data, sim_data = pe.calculate_inference_bounds(
+    dict_of_params, dict_of_responses, dict_of_controls
+)
+
+plt.plot(exp_data["x"], exp_data["f"], "ko", label="Exp. data")
+plt.plot(
+    exp_inference_results["x"],
+    exp_inference_results["f"]["simulation"],
+    "k-",
+    label="Sim. data",
+)
+plt.plot(
+    exp_inference_results["x"],
+    exp_inference_results["f"]["lower bound"],
+    "b:",
+    label="Lower bound",
+)
+plt.plot(
+    exp_inference_results["x"],
+    exp_inference_results["f"]["upper bound"],
+    "r:",
+    label="Upper bound",
+)
 plt.legend()
 plt.xlim(0, 1.2)
 plt.xlabel("Concentration")
@@ -64,3 +50,48 @@ plt.ylim(0, 250)
 plt.ylabel("Velocity")
 plt.grid()
 plt.show()
+
+dict_of_artificial_controls = {
+    "x": [0.0, 1.2, 10 + 1],
+}
+(
+    artificial_inference_results,
+    artificial_data,
+    artificial_sim_data,
+) = pe.calculate_inference_bounds(
+    dict_of_params,
+    dict_of_responses,
+    dict_of_controls,
+    dict_of_artificial_controls,
+    seed=42,
+)
+
+plt.plot(artificial_data["x"], artificial_data["f"], "ko", label="Artifial data")
+plt.plot(
+    artificial_inference_results["x"],
+    artificial_inference_results["f"]["simulation"],
+    "k-",
+    label="Sim. data",
+)
+plt.plot(
+    artificial_inference_results["x"],
+    artificial_inference_results["f"]["lower bound"],
+    "b:",
+    label="Lower bound",
+)
+plt.plot(
+    artificial_inference_results["x"],
+    artificial_inference_results["f"]["upper bound"],
+    "r:",
+    label="Upper bound",
+)
+plt.legend()
+plt.xlim(0, 1.2)
+plt.xlabel("Concentration")
+plt.ylim(0, 250)
+plt.ylabel("Velocity")
+plt.grid()
+plt.show()
+
+print(exp_inference_results["f"]["s"])
+print(artificial_inference_results["f"]["s"])
