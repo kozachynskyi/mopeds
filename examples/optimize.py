a53ea1e5b02cadca41aabbfc16f84eed78a0e186
Volodymyr Kozachynskyi <vovakozach@gmail.com>
2023-04-23 17:32:59 +0200

    refactor: reroll back example file changes

--- examples/optimize.py
+++ examples/optimize.py
@@ -10,22 +10,16 @@ if __name__ == "__main__":
 
     piecewiseswitch = False
     variable_list, m = par_est.examples.cstr_dae(piecewiseswitch)
-    res_dict = {}
     for var in variable_list.values():
         var.fixed = True
-        if isinstance(var, par_est.VariableParameter):
-            res_dict[var.name] = var.value[0]
 
     variable_list["e0_U"].fixed = False
-    variable_list["e0_E_r1"].fixed = False
-    variable_list["e0_E_r2"].fixed = False
-    variable_list["e0_E_r3"].fixed = False
     variable_list["e0_T_in"].fixed = False
     variable_list["e0_T"].variance = 0.1
 
     # Create time-grid. Zero should be first
-    time_grid1 = np.linspace(0, 1000, 2)
-    time_grid2 = np.linspace(0, 1000, 4)
+    time_grid1 = np.linspace(0, 1000, 4)
+    time_grid2 = np.linspace(0, 1000, 8)
 
     e0_T_in = variable_list["e0_T_in"]
     if isinstance(e0_T_in, par_est.VariableControlPiecewiseConstant):
@@ -36,33 +30,25 @@ if __name__ == "__main__":
 
     data1 = par_est.tools.generate_varlist_with_data(variable_list, m, time_grid1, True)
     data2 = par_est.tools.generate_varlist_with_data(variable_list, m, time_grid2, True)
-    data1.set_bounds(0.4)
     # data1.show()
 
     # If data is not available for all simulated points, PE works
-    for d in [data1, data2]:
-        e0_T = d["e0_T"]
-        e0_T.dataframe = e0_T._dataframe_from_value(e0_T.value[0])
+    e0_T = data2["e0_T"]
+    e0_T.dataframe = e0_T._dataframe_from_value(e0_T.value[0])
 
-    for i in range(1,2):
-        e0_c_i1_df = data2[f"e0_c_i{i}"].dataframe
-        e0_c_i1_df.drop(e0_c_i1_df.index[2:], inplace=True)
+    e0_c_i1_df = data2["e0_c_i1"].dataframe
+    e0_c_i1_df.drop(e0_c_i1_df.index[2:], inplace=True)
 
     # Perturbate alg variable from "ideal solution" to see its affect on PE
     a = data2["e0_c_tot"].dataframe
     data2["e0_c_tot"].dataframe = data2["e0_c_tot"].dataframe * 1.05
 
-    pe = par_est.ParameterEstimation(m, [data1, data2])
-    a = pe.calculate_objective_and_residual(res_dict, "wls")
-
-    res = pe.optimize()
-
-    breakpoint()
+    pe_state = par_est.ParameterEstimation(m, [data1, data2])
     # pe_state = par_est.ParameterEstimation(m, [data2])
     # print(pe_state.optimize(True))
 
     # pe_alg = par_est.ParameterEstimation(m, [data1, data2], use_algebraic_vars=True)
     # print(pe_alg.optimize(True))
 
-    # oed = par_est.OptimalExperimentalDesign(m, [data1], time_grid1)
-    # oed.optimize()
+    oed = par_est.OptimalExperimentalDesign(m, [data1], time_grid1)
+    oed.optimize()
