2daafc71531f1c0a2f1f33c14475e7867d9dab09
Volodymyr Kozachynskyi <vovakozach@gmail.com>
2025-03-04 17:52:57 +0100

    example: issue example fix

--- examples/issues/dae-optimization.py
+++ examples/issues/dae-optimization.py
@@ -1,78 +1,83 @@
-import mopeds
+try:
+    import mopeds
+    import numpy as np
+
+    def raults():
+        # https://webbook.nist.gov/cgi/cbook.cgi?ID=C7732185&Mask=4&Type=ANTOINE&Plot=on
+        # https://webbook.nist.gov/cgi/cbook.cgi?ID=C64175&Mask=4&Type=ANTOINE&Plot=on
+        variable_list = mopeds.VariableList()
+        # fmt:off
+        # e0_x_c1 = ca.MX.sym("e0_x_c1")
+        variable_list.add_variable(mopeds.VariableState("e0_x_c1", 0.1, 0, 1))  # noqa: E501
+        variable_list.add_variable(mopeds.VariableState("var_state2", 0, 0, 1))  # noqa: E501
+
+        variable_list.add_variable(mopeds.VariableAlgebraic("e0_P_LV_o_c1", 1.4144774629101626, 0.1, 10))  # noqa: E501
+        variable_list.add_variable(mopeds.VariableAlgebraic("e0_P_LV_o_c2", 0.3584844184269499, 0.1, 10))  # noqa: E501
+        variable_list.add_variable(mopeds.VariableParameter("e0_A_c1", 3.5595, 3, 4))  # noqa: E501
+        variable_list.add_variable(mopeds.VariableParameter("e0_B_c1", 643.748))  # noqa: E501
+        variable_list.add_variable(mopeds.VariableParameter("e0_C_c1", -198.043))  # noqa: E501
+        variable_list.add_variable(mopeds.VariableParameter("e0_A_c2", 4.92531, 4, 6))  # noqa: E501
+        variable_list.add_variable(mopeds.VariableParameter("e0_B_c2", 1432.526))  # noqa: E501
+        variable_list.add_variable(mopeds.VariableParameter("e0_C_c2", -61.819))  # noqa: E501
+
+        variable_list.add_variable(mopeds.VariableAlgebraic("e0_x_c2", 0.5, 0, 1))  # noqa: E501
+        variable_list.add_variable(mopeds.VariableAlgebraic("e0_y_c1", 0.78888, 0, 1))  # noqa: E501
+        variable_list.add_variable(mopeds.VariableAlgebraic("e0_y_c2", 0.21112, 0, 1))  # noqa: E501
+
+        # variable_list.add_variable(mopeds.VariableControlPiecewiseConstant("e0_x_c1", 0.1, 0, 1))  # noqa: E501
+
+        variable_list.add_variable(mopeds.VariableControl("e0_T", 346.4149, 100, 500))  # noqa: E501
+        variable_list.add_variable(mopeds.VariableAlgebraic("e0_P", 1.013, 0.1, 6))  # noqa: E501
+
+        m = mopeds.Model(variable_list)
+
+        e0_P = m.varlist_all["e0_P"].casadi_var  # noqa: E501
+        e0_x_c1 = m.varlist_all["e0_x_c1"].casadi_var  # noqa: E501
+        e0_P_LV_o_c1 = m.varlist_all["e0_P_LV_o_c1"].casadi_var  # noqa: E501
+        e0_P_LV_o_c2 = m.varlist_all["e0_P_LV_o_c2"].casadi_var  # noqa: E501
+        e0_A_c1 = m.varlist_all["e0_A_c1"].casadi_var  # noqa: E501
+        e0_A_c2 = m.varlist_all["e0_A_c2"].casadi_var  # noqa: E501
+        e0_B_c1 = m.varlist_all["e0_B_c1"].casadi_var  # noqa: E501
+        e0_B_c2 = m.varlist_all["e0_B_c2"].casadi_var  # noqa: E501
+        e0_C_c1 = m.varlist_all["e0_C_c1"].casadi_var  # noqa: E501
+        e0_C_c2 = m.varlist_all["e0_C_c2"].casadi_var  # noqa: E501
+        e0_T = m.varlist_all["e0_T"].casadi_var  # noqa: E501
+        e0_x_c2 = m.varlist_all["e0_x_c2"].casadi_var  # noqa: E501
+        e0_y_c1 = m.varlist_all["e0_y_c1"].casadi_var  # noqa: E501
+        e0_y_c2 = m.varlist_all["e0_y_c2"].casadi_var  # noqa: E501
+
+        eq_s = 0.1
+        eq_s2 = e0_y_c1
+        EQ_alg1 = ((e0_y_c1*e0_P)-((e0_x_c1*e0_P_LV_o_c1)))  # noqa: E501,E226
+        EQ_alg2 = ((e0_y_c2*e0_P)-((e0_x_c2*e0_P_LV_o_c2)))  # noqa: E501,E226
+        EQ_alg3 = (1.0-(((e0_x_c1+e0_x_c2))))  # noqa: E501,E226
+        EQ_alg4 = (1.0-(((e0_y_c1+e0_y_c2))))  # noqa: E501,E226
+
+        EQ_alg5 = (e0_P_LV_o_c1-10**(e0_A_c1 - e0_B_c1 / (e0_T + e0_C_c1)))  # noqa: E501,E226
+        EQ_alg6 = (e0_P_LV_o_c2-10**(e0_A_c2 - e0_B_c2 / (e0_T + e0_C_c2)))  # noqa: E501,E226
+
+        list_algebraic_equations = [EQ_alg1, EQ_alg2, EQ_alg3, EQ_alg4, EQ_alg5, EQ_alg6]  # noqa: E501
+        # fmt:on
+
+        m.add_equations_differential([eq_s, eq_s2])
+        m.add_equations_algebraic(list_algebraic_equations)
+
+        return variable_list, m
+    vl, m = raults()
+    grid = np.linspace(0, 8, 2)
+    sim = mopeds.Simulator(m, grid, vl)
+    res_mopeds = sim.simulate(algebraic=True)[2]
+except Exception:
+    pass
+
+
 import casadi as ca
 import numpy as np
 import copy
 
 
-def raults():
-    # https://webbook.nist.gov/cgi/cbook.cgi?ID=C7732185&Mask=4&Type=ANTOINE&Plot=on
-    # https://webbook.nist.gov/cgi/cbook.cgi?ID=C64175&Mask=4&Type=ANTOINE&Plot=on
-    variable_list = mopeds.VariableList()
-    # fmt:off
-    # e0_x_c1 = ca.MX.sym("e0_x_c1")
-    variable_list.add_variable(mopeds.VariableState("e0_x_c1", 0.1, 0, 1))  # noqa: E501
-    variable_list.add_variable(mopeds.VariableState("var_state2", 0, 0, 1))  # noqa: E501
-
-    variable_list.add_variable(mopeds.VariableAlgebraic("e0_P_LV_o_c1", 1.4144774629101626, 0.1, 10))  # noqa: E501
-    variable_list.add_variable(mopeds.VariableAlgebraic("e0_P_LV_o_c2", 0.3584844184269499, 0.1, 10))  # noqa: E501
-    variable_list.add_variable(mopeds.VariableParameter("e0_A_c1", 3.5595, 3, 4))  # noqa: E501
-    variable_list.add_variable(mopeds.VariableParameter("e0_B_c1", 643.748))  # noqa: E501
-    variable_list.add_variable(mopeds.VariableParameter("e0_C_c1", -198.043))  # noqa: E501
-    variable_list.add_variable(mopeds.VariableParameter("e0_A_c2", 4.92531, 4, 6))  # noqa: E501
-    variable_list.add_variable(mopeds.VariableParameter("e0_B_c2", 1432.526))  # noqa: E501
-    variable_list.add_variable(mopeds.VariableParameter("e0_C_c2", -61.819))  # noqa: E501
-
-    variable_list.add_variable(mopeds.VariableAlgebraic("e0_x_c2", 0.5, 0, 1))  # noqa: E501
-    variable_list.add_variable(mopeds.VariableAlgebraic("e0_y_c1", 0.78888, 0, 1))  # noqa: E501
-    variable_list.add_variable(mopeds.VariableAlgebraic("e0_y_c2", 0.21112, 0, 1))  # noqa: E501
-
-    # variable_list.add_variable(mopeds.VariableControlPiecewiseConstant("e0_x_c1", 0.1, 0, 1))  # noqa: E501
-
-    variable_list.add_variable(mopeds.VariableControl("e0_T", 346.4149, 100, 500))  # noqa: E501
-    variable_list.add_variable(mopeds.VariableAlgebraic("e0_P", 1.013, 0.1, 6))  # noqa: E501
-
-    m = mopeds.Model(variable_list)
-
-    e0_P = m.varlist_all["e0_P"].casadi_var  # noqa: E501
-    e0_x_c1 = m.varlist_all["e0_x_c1"].casadi_var  # noqa: E501
-    e0_P_LV_o_c1 = m.varlist_all["e0_P_LV_o_c1"].casadi_var  # noqa: E501
-    e0_P_LV_o_c2 = m.varlist_all["e0_P_LV_o_c2"].casadi_var  # noqa: E501
-    e0_A_c1 = m.varlist_all["e0_A_c1"].casadi_var  # noqa: E501
-    e0_A_c2 = m.varlist_all["e0_A_c2"].casadi_var  # noqa: E501
-    e0_B_c1 = m.varlist_all["e0_B_c1"].casadi_var  # noqa: E501
-    e0_B_c2 = m.varlist_all["e0_B_c2"].casadi_var  # noqa: E501
-    e0_C_c1 = m.varlist_all["e0_C_c1"].casadi_var  # noqa: E501
-    e0_C_c2 = m.varlist_all["e0_C_c2"].casadi_var  # noqa: E501
-    e0_T = m.varlist_all["e0_T"].casadi_var  # noqa: E501
-    e0_x_c2 = m.varlist_all["e0_x_c2"].casadi_var  # noqa: E501
-    e0_y_c1 = m.varlist_all["e0_y_c1"].casadi_var  # noqa: E501
-    e0_y_c2 = m.varlist_all["e0_y_c2"].casadi_var  # noqa: E501
-
-    eq_s = 0.1
-    eq_s2 = 1
-    EQ_alg1 = ((e0_y_c1*e0_P)-((e0_x_c1*e0_P_LV_o_c1)))  # noqa: E501,E226
-    EQ_alg2 = ((e0_y_c2*e0_P)-((e0_x_c2*e0_P_LV_o_c2)))  # noqa: E501,E226
-    EQ_alg3 = (1.0-(((e0_x_c1+e0_x_c2))))  # noqa: E501,E226
-    EQ_alg4 = (1.0-(((e0_y_c1+e0_y_c2))))  # noqa: E501,E226
-
-    EQ_alg5 = (e0_P_LV_o_c1-10**(e0_A_c1 - e0_B_c1 / (e0_T + e0_C_c1)))  # noqa: E501,E226
-    EQ_alg6 = (e0_P_LV_o_c2-10**(e0_A_c2 - e0_B_c2 / (e0_T + e0_C_c2)))  # noqa: E501,E226
-
-    list_algebraic_equations = [EQ_alg1, EQ_alg2, EQ_alg3, EQ_alg4, EQ_alg5, EQ_alg6]  # noqa: E501
-    # fmt:on
-
-    m.add_equations_differential([eq_s, eq_s2])
-    m.add_equations_algebraic(list_algebraic_equations)
-
-    return variable_list, m
-
-vl, m = raults()
-grid = np.linspace(0, 8, 2)
-sim = mopeds.Simulator(m, grid, vl)
-res_mopeds = sim.simulate(algebraic=True)[2]
-
 ISSUE_1_trigger = False
-ISSUE_2_trigger = True
+ISSUE_2_trigger = False
 
 def integrate_over_time_grid(time_grid, integrator, y_0, z_0, p_0):
     x0 = y_0
@@ -182,8 +187,8 @@ solver = ca.nlpsol("par_est", "ipopt", nlpsol_dict, {"monitor": "nlp_grad_f", "i
 
 nlpsol_args = { "x0": 3.4, "lbx": 3, "ubx": 4}
 
-res = solver.call(nlpsol_args)
-print(f"Expected 3.5595, estimated: {res['x']}")
+res_pe = solver.call(nlpsol_args)
+print(f"Expected 3.5595, estimated: {res_pe['x']}")
 
 final_res = function(3.5595)
 res_obj = obj_function(3.5595)
@@ -192,7 +197,6 @@ jac = obj_jac(3.4, 0)
 print(jac)
 
 print(res_obj)
-breakpoint()
 
 # Test if model correctly impelemnted, do not submit to issue
 assert np.isclose(res_mopeds.dataframe.to_numpy(), res.T).all()
